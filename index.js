fe99a20f1b0c91d43a9d572ff6ba80ba4f0a61fb
ATAGWE ROGER <atagweroger2@gmail.com>
2025-07-08 10:03:03 +0100

    Rewrote the code

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
-  let filePath = '';
-  let contentType = 'text/html';
+const server = http.createServer()
+server.on('request', (request, response) => {
+  let filePath = ''
+  let contentType = 'text/html'
 
-  if (req.url === '/' || req.url === '/index') {
-    filePath = path.join(__dirname, 'index.html');
-  } else if (req.url === '/about') {
-    filePath = path.join(__dirname, 'about.html');
-  } else if (req.url === '/contact-me') {
-    filePath = path.join(__dirname, 'contact-me.html');
-  } else if (req.url === '/style.css') {
-    filePath = path.join(__dirname, 'style.css');
-    contentType = 'text/css';
-  } else {
-    filePath = path.join(__dirname, '404.html');
-    res.statusCode = 404;
+  if(request.url === '/' || request.url === '/index'){
+    filePath = path.join(__dirname, 'index.html')
+  }else if(request.url === '/about'){
+    filePath = path.join(__dirname, 'about.html')
+  }else if(request.url === '/contact-me'){
+    filePath = path.join(__dirname, 'contact-me.html')
+  }else if(request.url === '/style.css'){
+    filePath = path.join(__dirname, 'style.css')
+    contentType = 'text/css'
+  }
+  else{
+    filePath = path.join(__dirname, '404.html')
   }
 
-  // Read and serve the file
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end('Server Error!');
-    } else {
-      res.writeHead(res.statusCode || 200, { 'Content-Type': contentType });
-      res.end(data);
+  fs.readFile(filePath, (err, data ) => {
+    if(err){
+      response.writeHead(500);
+      response.end(data)
+      return;
     }
-  });
-});
+    response.writeHead(200, {'content-type': contentType})
+    response.end(data)
 
-server.listen(8080, () => {
-  console.log('Server running at http://localhost:8080');
-});
+  })
+})
 
+server.listen(7070, () => {
+  console.log('Code now running on localhost:7070')
+})
